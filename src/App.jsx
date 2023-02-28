771b0fc01a27f55dc26c1870b727def950b6f7ac
raxhavijaya <raxhavijaya31@gmail.com>
2023-03-01 01:43:45 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./App.css";
+import Home from "./components/home/Home";
+import Services from "./components/services/Services";
+
+function App() {
+  return <main className="main">
+    <Home/>
+    <Services/>
+  </main>;
+}
+
+export default App;
